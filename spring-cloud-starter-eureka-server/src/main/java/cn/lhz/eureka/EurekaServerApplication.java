7e9662f33dc3400a1796612e124bb64f0e22a232
刘鸿哲 <wb-lhz330051@alibaba-inc.com>
2018-01-12 10:45:54 +0800

    Initial commit

---
+++ spring-cloud-starter-eureka-server/src/main/java/cn/lhz/eureka/EurekaServerApplication.java
@@ -0,0 +1,18 @@
+package cn.lhz.eureka;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+/**
+ * @auther liu_hz
+ * @create 2018/1/12 9:53
+ */
+@SpringBootApplication
+@EnableEurekaServer
+public class EurekaServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EurekaServerApplication.class, args);
+    }
+}
